3d0bc7d322ae98166deb98d930609d183313aa5e
unknown <miguelmorenojurado@gmail.com>
2020-03-07 16:35:45 +0100

    primera version

---
+++ contacto.php
@@ -0,0 +1,15 @@
+<?php
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $content=$_POST['content'];
+
+
+    echo "Tu nombre es ".$name."<br>";
+    echo "Tu e-mail es ".$email."<br>";
+    echo "Tu mensaje es ".$content."<br>";
+
+
+
+
+?>
